49e5bec03131af7b5b3f9c7c17e0957410bcb4a1
Thuchanthini <thuchanthinis@gmail.com>
2026-05-23 16:04:55 +0530

    Add files via upload

---
+++ dataextract.py
@@ -0,0 +1,14 @@
+import pandas as pd
+df = pd.read_csv('netflix_titles.csv')
+
+import sqlalchemy as sal
+engine = sal.create_engine(r'mssql://THUCHANTHINI\SQLEXPRESS/master?driver=ODBC+DRIVER+17+FOR+SQL+SERVER', fast_executemany=True)
+conn = engine.connect()
+
+df.to_sql('net_raw', con=conn, index=False, if_exists='append')
+conn.close()
+
+df.head()
+df[df.show_id == 's5023']
+max(df.title.str.len())
+df.isna().sum()
